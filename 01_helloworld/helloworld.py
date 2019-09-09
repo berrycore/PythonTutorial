94478b685bd3810a46bf90810f012a0c530b4c55
SanghoonJeong <berrycore@gmail.com>
2019-09-09 23:25:45 +0900

    folder updated

--- 01_helloworld/helloworld.py
+++
@@ -1,6 +0,0 @@
-print("hello world")
-print("hello world updated " * 3)
-# I`m comment
-message = "# I`m not comment"
-print(message)
-
---
+++ Start/ex2_print.py
@@ -0,0 +1,8 @@
+print(10,20,30,40,50)
+print()
+print()
+print()
+# I`m comment
+message = "# I`m not comment"
+print(message)
+print("hello world updated " * 3)
