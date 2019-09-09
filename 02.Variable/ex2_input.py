53041aadfb529c9317400a4a4ad2a3d01afe6fee
SanghoonJeong <berrycore@gmail.com>
2019-09-10 00:36:51 +0900

    string format

--- 02.Variable/ex2_input.py
+++ 02.Variable/ex2_input.py
@@ -2,22 +2,3 @@
 cmd = input("command : ")
 print(cmd)
 
-# convert data type (str)
-a = input("input 1st number : ") # if a = "10"
-b = input("input 2nd number : ") # if b = "20"
-print(a + b)                     # "1020"
-
-# convert data type (str -> int)
-a = int(input("input 1st number : ")) # if a = 10
-b = int(input("input 2nd number : ")) # if b = 20
-print(a + b)                          # 30
-
-# convert data type (int -> str)
-a = str(a)                       # 10 -> "10"
-b = str(b)                       # 20 -> "20"
-print(a + b)                     # "1020"
-
-# convert data type (str -> float)
-s = "10"
-s = float(s)
-print(s)                        # "10" -> 10.0
---
+++ 02.Variable/ex4_string_format.py
@@ -0,0 +1,9 @@
+msg1 = "HelloWorld!".lower()
+msg2 = "HelloWorld!".upper()
+
+print(msg1)
+print(msg2)
+
+date = "{}Year, {}Month, {}Day".format(2019, 9, 4)
+
+print(date)
