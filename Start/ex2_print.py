aea1edb2a0f32c58f22ea4ef3378d468b4cd24fe
SanghoonJeong <berrycore@gmail.com>
2019-09-10 00:01:16 +0900

    operator updated

--- Start/ex2_print.py
+++ Start/ex2_print.py
@@ -6,3 +6,33 @@ print()
 message = "# I`m not comment"
 print(message)
 print("hello world updated " * 3)
+# escape character
+print("\"HI\"")
+print("\tTAB")
+print("\nLINE FEED")
+print("""Hello
+Hello
+Hello""")
+print("-" * 20)
+print("Hello" + "World")
+
+print("Hello"[0])
+print("Hello"[1])
+print("Hello"[2])
+print("Hello"[3])
+print("Hello"[4])
+print("Hello"[-1])
+print("Hello"[-2])
+print("Hello"[-3])
+print("Hello"[-4])
+print("-" * 20)
+print("HelloWorld!"[0:1])
+print("HelloWorld!"[0:2])
+print("HelloWorld!"[0:3])
+print("HelloWorld!"[0:4])
+print("HelloWorld!"[0:5])
+print("-" * 20)
+print("HelloWorld!"[:5])
+print("HelloWorld!"[5:])
+print("-" * 20)
+print("HelloWorld!"[-1:-2]) # Nothing was printed out
---
+++ Start/ex3_type.py
@@ -0,0 +1,7 @@
+print(type(10))
+print(type(10.0))
+print(type(10.0 + 1j))
+print(type("message"))
+print(type('msg'))
+
+
